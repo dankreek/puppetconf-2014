5f8549a85dbc85fc85ac7dfcc67a8f27a49afc2b
Justin May <justin.may@puppetlabs.com>
2014-09-09 16:41:56 -0700

    Gatling no longer needs su

--- start-gatling.sh
+++ start-gatling.sh
@@ -1,5 +1,5 @@
 
-ssh gatling "su gatling - -c \"cd /home/gatling/gatling-puppet-load-test/gatling2-sims/; ./run.sh\""
+ssh gatling "cd /home/gatling/gatling-puppet-load-test/gatling2-sims/; ./run.sh"
 
 echo "done."
 
