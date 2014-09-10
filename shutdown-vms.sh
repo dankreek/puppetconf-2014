25a274af9116693de582dc6495017d5534fb85cb
Justin May <justin.may@puppetlabs.com>
2014-09-10 15:27:45 -0700

    Using more universal shutdown command

--- shutdown-vms.sh
+++ shutdown-vms.sh
@@ -1,15 +1,15 @@
 shutdown () {
     echo "Shutting down $1"
-    ssh root@$1 "shutdown now"
+    ssh root@$1 "init 0"
 }
 
 echo "Shutting down all hosts."
 
-shutdown grafana
-shutdown puppetmaster1
-shutdown puppetmaster2
-shutdown haproxy
-shutdown gatling
+shutdown grafana & \
+shutdown puppetmaster1 & \
+shutdown puppetmaster2 & \
+shutdown haproxy & \
+shutdown gatling & \
 
 echo "done."
 
